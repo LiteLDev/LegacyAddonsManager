9db774c934f4cb020731bc5d342888d8c1954fb9
ShrBox <shrbox@outlook.com>
2024-03-27 10:35:42 +0800

    chore: update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 
-add_requires("levilamina")
+add_requires("levilamina 0.10.0")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
